196b8742d6219e7b534b9bd730b217a38986fea3
Jidlafe Hegner <jidlafehegner@gmail.com>
2020-06-01 01:23:00 -0300

    Home component do website module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { WebsiteModule } from './website/website.module';
+import { PortalModule } from './portal/portal.module';
+import { HomeComponent } from './website/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, WebsiteModule, PortalModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
